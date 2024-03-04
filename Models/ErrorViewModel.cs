08a2b8f952a69d2bc2fc492c4c6726079150de13
MathildaEriksson <tilla_89@hotmail.com>
2024-03-04 14:42:39 +0100

    Gör om, gör rätt

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace EquiMarketApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
